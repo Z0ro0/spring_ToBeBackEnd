22552ea2114235b4b57acf4e5894a83f472d1af4
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-20 11:55:39 +0900

    @SpringBootApplication 살펴보기

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//스프링 부트 사용에 필요한 기본 설정 해 줌
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -3,10 +3,10 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController//라우처 역할, 클라이언트 요청에 맞는 메서드 실행 가능
 public class TestController {
-    //localhost:1234/test 접속 시 Hello World 보임 
-    @GetMapping("/test")
+    //localhost:1234/test 접속 시 Hello World 보임
+    @GetMapping("/test")// /test GET 요청이 오면 test() 메서드 실행
     public String test(){
         return "Hello world";
     }
